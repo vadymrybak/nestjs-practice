d4cdc3dba9d85c81e44625389ac91c97ec347dc8
Vadym Rybak <vadymrybak@outlook.com>
2024-01-30 22:52:31 +0300

    EOD

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller("auth")
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    // POST /auth/signup
+    @Post("signup")
+    signup() {
+        return this.authService.signin();
+    }
+
+    @Post("signin")
+    signin() {
+        return this.authService.signup();
+    }
+}
