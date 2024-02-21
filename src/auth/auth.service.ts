903545d30c52adf606f7e56fcc4dca8759589e60
Vadym Rybak <vadymrybak@outlook.com>
2024-02-20 21:07:48 -0500

    EOD

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,64 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-    signin() {
-        return { msg: "I am signed in" }
+    constructor(private prisma: PrismaService) { }
+    async signin(dto: AuthDto) {
+        // Find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        });
+
+        // If user does not exist - throw exception
+        if (!user) {
+            throw new ForbiddenException("Credential incorrect");
+        }
+        // Compare password
+
+        const pwMatches = await argon.verify(user.hash, dto.password);
+
+        // If pass incorrect - throw excpetion
+        if (!pwMatches) {
+            throw new ForbiddenException("Credentials incorect");
+        }
+
+        delete user.hash;
+
+        return user;
     }
 
-    signup() {
-        return { msg: "I am signed up" }
+    async signup(dto: AuthDto) {
+        // generate password
+        const hash = await argon.hash(dto.password);
+
+        // save user to db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            });
+
+            delete user.hash;
+
+            // return user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === "P2002") {
+                    throw new ForbiddenException("Credentials taken");
+                }
+            }
+            throw error;
+        }
+
     }
 }
